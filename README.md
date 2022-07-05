3247f2b0a5f89804cfebf572b0224d794912f4d5
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2022-07-05 11:27:07 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# FilterData-by-IndivTime
+# Filter Data by Indiv Time
+MoveApps
+
+Github repository: *github.com/movestore/FilterData-by-IndivTime*
+
+## Description
+Filters your data set by track/individual specific time ranges that are provided as table/move object.
+
+## Documentation
+This App requires as input a data set consisting of tracks and one track (called e.g. 'nest') that containes information about track specific start and end timestamps by which the tracks shall be filtered. Note that the names of the tracks and those in the e.g. 'nest' table must be equal. Variable names must be given in the exact spelling as in the data, refer to the Track Attributes in the data summary of the previous App.
+
+The use case for which this App was built is to filter data for nesting times. The nest object is generated in the Nest Detection App and can be appended to tracks with the same track names by the Cloud Storage App.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+none
+
+### Parameters 
+`selVar`: Name of the object containing the start and end timestamps. E.g. `nest` when using rds output from Nest Detection App.
+
+`trackVar`: Name of the track ID variable in the object containing the start and end timestamps. Take care that this parameter also exists in the track attributes of the input data set.
+
+`startVar`: Name of the start time variable. Take care that this parameter also exists in the track attributes of the input data set. Times must be provided in the format `YYYY-MM-DD` (if date) or `YYYY-MM-DD HH:MM:SS.SSS`.
+
+`endVar`: Name of the end time variable. Take care that this parameter also exists in the track attributes of the input data set. Times must be provided in the format `YYYY-MM-DD` (if date) or `YYYY-MM-DD HH:MM:SS.SSS`.
+
+
+### Null or error handling:
+**Parameter `selVar`:** If there is no object with the given name the App will run into an error.
+
+**Parameter `trackVar`:** If there is no variable with the name given here, an error will be returned.
+
+**Parameter `startVar`:** If there is no variable with the name given here, the individual track will not be filtered in this direction.
+
+**Parameter `endVar`:** If there is no variable with the name given here, the individual track will not be filtered in this direction.
+
+**Data:** If none of the locations in the input data set are in the required time intervals, a warning is given and a NULL data set is returned. 
---
+++ RFunction.R
@@ -0,0 +1,78 @@
+library('move')
+library('foreach')
+
+rFunction <- function(data, selName=NULL, startVar=NULL, endVar=NULL, trackVar="individual.local.identifier")
+{
+  Sys.setenv(tz="UTC") 
+  
+  if (is.null(selName) | (selName %in% namesIndiv(data))==FALSE) 
+  {
+    logger.info("Your property name (e.g. 'nest') is not set or does not exist in the data. This will lead to an error.")
+    result <- NULL
+  } else
+  {
+    ix <- which(namesIndiv(data)==selName)
+    selT <- data[[ix]]
+    data <- data[[-ix]]
+   
+    data.split <- move::split(data)
+    
+    data_filter <- foreach(datai = data.split) %do% {
+      id <- namesIndiv(datai)
+      logger.info(id)
+      
+      minT <- min(timestamps(datai))
+      maxT <- max(timestamps(datai))
+      
+      if (is.null(startVar) | (startVar %in% names(datai))==FALSE)
+      {
+        time1 <- minT
+        logger.info(paste("You have not provided a starting time variable name or the provided names does not exist in the data. Therefore, each track is filtered by its minimum timestamp."))
+      } else 
+      {
+        if (any(selT@data$individual.local.identifier==id)) time1 <- as.POSIXct(selT@data[which(selT@data[,trackVar]==id),startVar]) else time1 <- NA
+      }
+      
+      if (is.null(endVar) | (endVar %in% names(datai))==FALSE)
+      {
+        timeN <- maxT
+        logger.info(paste("You have not provided an end time variable name or the provided names does not exist in the data. Therefore, each track is filtered by its maximum timestamp."))
+      } else 
+      {
+        if (any(selT@data$individual.local.identifier==id)) timeN <- as.POSIXct(selT@data[which(selT@data[,trackVar]==id),endVar]) else timeN <- NA  
+      }
+      
+      if (is.na(time1) | is.na(timeN)) dataif <- NULL else
+      {
+        if (time1>timeN)
+        {
+          time10 <- time1
+          timeN0 <- timeN
+          time1 <- timeN0
+          timeN <- time10
+          logger.info("Warning! Error! Your start timestamp is after your end timestamp. We assume that they were switched and filter the data accordingly.")
+        }
+        
+        if (minT>timeN | maxT<time1)
+        {
+          logger.info("Warning! None of your data lies in the requested time range. Return NULL.")
+          dataif <- NULL
+        } else
+        {
+          dataif <- datai[timestamps(datai)>=time1 & timestamps(datai)<=timeN,]
+          logger.info(paste("Filtering successful. It found",length(dataif),"positions of",length(datai),"locations for the track",id,"."))
+        }
+      }
+
+      dataif
+    }
+    names(data_filter) <- selT@data[,trackVar]
+    
+    data_filter.nozero <- data_filter[unlist(lapply(data_filter, length) > 0)]
+    result <- moveStack(data_filter.nozero,forceTz="UTC")
+
+  }
+
+  return(result)
+}
+  
